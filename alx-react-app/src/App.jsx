3cdd62acd035263c5224897a616cee58f79d7866
me <lorraobare@gmail.com>
2025-11-05 10:06:14 +0300

    created different components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,8 +15,17 @@ function App() {
       <div>
         <WelcomeMessage />
         </div>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <MainContent/>
+        </div>
+        <div>
+          <footer/>
+        </div>
     </>
-  )
+  );
 }
 
 export default App
