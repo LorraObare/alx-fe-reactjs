f526b8c71942c3cb8dc3037955d70c4d2cf010de
me <lorraobare@gmail.com>
2025-11-05 09:53:14 +0300

    importation of WelcomeMessage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
 
@@ -6,8 +8,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <WelcomeMessage/>
-
+    <>
+      <div>
+        <WelcomeMessage />
+        </div>
+    </>
   )
 }
 
