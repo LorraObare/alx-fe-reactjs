bb252d1f5b6907146c24306168c991c8737c355f
me <lorraobare@gmail.com>
2025-11-09 08:54:41 +0300

    refactoring

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,4 +1,5 @@
 import { createContext } from "react";
 
-// Create and export the context
-export const UserContext = createContext(null);
+const UserContext = createContext(null);
+
+export default UserContext;
